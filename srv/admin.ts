f65324ae8084fd46e2bedea82fae7a0bf59fff6e
Aryan Raj Sinha <aryan.raj.sinha@sap.com>
2024-03-03 06:39:07 +0000

    trying cicd again 2

--- srv/admin.ts
+++ srv/admin.ts
@@ -40,7 +40,7 @@ class AdminService extends ApplicationService {
     return this.bupaSrv.run(req.query)
   }
 
-  //Comment
+  
 
   private handleBusinessPartnerCreated = async (msg: Request) => {
     const { BusinessPartner: businessPartnerID } = msg.data as IEventData
